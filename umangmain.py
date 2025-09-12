eec17b79c3bb511a4598cf76782e5bc5c25d2d4b
Patel, Umang Sarjukumar <umangsarjukumarpatel@my.unt.edu>
2025-09-12 09:53:14 -0500

    Initial setup with RSA key generation and host/port variables

--- umangmain.py
+++ umangmain.py
@@ -1,1 +1,9 @@
 #Umang Patel CSCE3550 Project1
+from cryptography.hazmat.primitives.asymmetric import rsa as myrsa
+
+host= "127.0.0.1"
+port=8080
+
+privatekey=myrsa.generate_private_key (key_size=2048,public_exponent=65537)
+expiredkey=myrsa.generate_private_key(key_size=2048,public_exponent=65537)
+
